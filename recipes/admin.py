6553c0b19ad946c2ba859acea0390bebdfbc75c9
Jonathan William da Silva <jonathanwilliam.ws@gmail.com>
2022-09-13 17:07:02 -0300

    inserted  recipes in database

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,10 +1,18 @@
 from django.contrib import admin
 
-from .models import Category
+from .models import Category, Recipe
 
 
 class CategoryAdmin(admin.ModelAdmin):
     ...
 
+# maineira mais curta de registrar um model
 
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    ...
+
+
+# maneira com mais passos de registrar um model
 admin.site.register(Category, CategoryAdmin)
