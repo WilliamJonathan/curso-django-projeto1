e2e2d9a0eb6b09cefe9a9363350b4423b127b2f5
Jonathan William da Silva <jonathanwilliam.ws@gmail.com>
2022-09-14 11:54:49 -0300

    bug fixes

--- recipes/views.py
+++ recipes/views.py
@@ -1,31 +1,48 @@
-from django.shortcuts import render
-from util.recipes.factory import make_recipe
+from django.shortcuts import get_list_or_404, get_object_or_404, render
 
-from .models import Recipe
+from recipes.models import Recipe
 
 
+# renderiza pagina home
 def home(request):
+
     recipes = Recipe.objects.filter(
         is_published=True
     ).order_by('-id')
+
     return render(request, 'recipes/pages/home.html', context={
         # 'recipes': [make_recipe() for _ in range(10)],
         'recipes': recipes,
     })
 
 
+# renderiza pagina de categorias das receitas
 def category(request, category_id):
-    recipes = Recipe.objects.filter(
-        category__id=category_id,
-        is_published=True,
-    ).order_by('-id')
+
+    recipes = get_list_or_404(
+        Recipe.objects.filter(
+            category__id=category_id,
+            is_published=True,
+        ).order_by('-id')
+    )
+
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
+        'title': f'{recipes[0].category.name}',
     })
 
 
+# renderiza pagina de detalhes de uma receita
 def recipe(request, id):
+
+    # recipe = Recipe.objects.filter(
+    #     pk=id,
+    #     is_published=True,
+    # ).order_by('-id').first()
+
+    recipe = get_object_or_404(Recipe, pk=id, is_published=True,)
+
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'recipe': make_recipe(),
+        'recipe': recipe,
         'is_detail_page': True,
     })
