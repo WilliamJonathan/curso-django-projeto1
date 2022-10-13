e05c18555e820155e85ce5581b10988bbba5690a
Jonathan William da Silva <jonathanwilliam.ws@gmail.com>
2022-10-13 16:23:17 -0300

    validate forms

--- authors/forms.py
+++ authors/forms.py
@@ -1,8 +1,49 @@
 from django import forms
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+
+
+def add_attr(field, attr_name, attr_new_val):
+    existing = field.widget.attrs.get(attr_name, '')
+    field.widget.attrs[attr_name] = f'{existing} {attr_new_val}'.strip()
+
+
+def add_placeholder(field, placeholder_val):
+    add_attr(field, 'placeholder', placeholder_val)
 
 
 class RegisterForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        add_placeholder(self.fields['username'], 'Seu apelido')
+        add_placeholder(self.fields['email'], 'Seu e-mail')
+        add_placeholder(self.fields['first_name'], 'Ex.: John')
+        add_placeholder(self.fields['last_name'], 'Ex.: da Silva')
+        add_attr(self.fields['username'], 'css', 'a-css-class')
+
+    password = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Sua senha'
+        }),
+        label='Senha',
+        error_messages={
+            'required': 'Senha não pode estar em branco'
+        },
+        help_text=(
+            'A senha deve ter pelo menos uma letra maiúscula, '
+            'uma letra minúscula e um número. O comprimento deve ter '
+            'pelo menos 8 caracteres.'
+        )
+    )
+    password2 = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Confirme sua senha'
+        }),
+        label='Confirmar senha',
+    )
+
     class Meta:
         model = User
         fields = [
@@ -13,13 +54,12 @@ class RegisterForm(forms.ModelForm):
             'password',
         ]
         # exclude = ['first_name]
-        labels = {
-            'first_name': 'Nome',
-            'last_name': 'Sobre nome',
-            'username': 'Apelido',
-            'email': 'E-mail',
-            'password': 'Senha',
-        }
+        # labels = {
+        #     'first_name': 'Nome',
+        #     'last_name': 'Sobre nome',
+        #     'username': 'Apelido',
+        #     'email': 'E-mail',
+        # }
         help_texts = {
             'email': 'Precisa ser um email valido'
         }
@@ -28,11 +68,35 @@ class RegisterForm(forms.ModelForm):
                 'required': 'Campo não pode estar em branco',
             }
         }
-        widgets = {
-            'first_name': forms.TextInput(attrs={
-                'placeholder': 'Informe seu primeiro nome aqui'
-            }),
-            'password': forms.PasswordInput(attrs={
-                'placeholder': 'Escolha uma senha'
-            })
-        }
+        # widgets = {
+        #     'first_name': forms.TextInput(attrs={
+        #         'placeholder': 'Informe seu primeiro nome aqui'
+        #     }),
+        #     'password': forms.PasswordInput(attrs={
+        #         'placeholder': 'Escolha uma senha'
+        #     })
+        # }
+
+    def clean_password(self):
+        data = self.cleaned_data.get('password')
+
+        if 'atenção' in data:
+            raise ValidationError(
+                'Não digite %(pipoca)s no campo password',
+                code='invalid',
+                params={'pipoca': '"atenção"'}
+            )
+
+        return data
+
+    def clean_first_name(self):
+        data = self.cleaned_data.get('first_name')
+
+        if 'John Doe' in data:
+            raise ValidationError(
+                'Não digite %(value)s no campo first name',
+                code='invalid',
+                params={'value': '"John Doe"'}
+            )
+
+        return data
