a691c67f524936f025f7a9e47b147dee36cadddc
Jonathan William da Silva <jonathanwilliam.ws@gmail.com>
2022-10-19 15:34:52 -0300

    test input regiters

--- tests/functional_tests/authors/test_authors_register.py
+++ tests/functional_tests/authors/test_authors_register.py
@@ -18,23 +18,87 @@ class AuthorsRegisterTest(AuthorsBaseTest):
             if field.is_displayed():
                 field.send_keys(' ' * 20)
 
-    def test_empty_first_name_error_message(self):
-        self.browser.get(self.live_server_url + '/authors/register/')
-        form = self.browser.find_element(
+    def get_form(self):
+        return self.browser.find_element(
             By.XPATH,
             '/html/body/main/div[2]/form'
         )
+
+    def form_field_test_with_callback(self, callback):
+        self.browser.get(self.live_server_url + '/authors/register/')
+        form = self.get_form()
 
         self.fill_form_dummy_data(form)
         form.find_element(By.NAME, 'email').send_keys('dummy@email.com')
 
-        first_name_field = self.get_by_placeholder(form, 'Ex.: John')
-        first_name_field.send_keys(' ')
-        first_name_field.send_keys(Keys.ENTER)
+        callback(form)
+        return form
 
-        form = self.browser.find_element(
-            By.XPATH,
-            '/html/body/main/div[2]/form'
+    def test_empty_first_name_error_message(self):
+        def callback(form):
+            first_name_field = self.get_by_placeholder(form, 'Ex.: John')
+            first_name_field.send_keys(' ')
+            first_name_field.send_keys(Keys.ENTER)
+            form = self.get_form()
+            self.assertIn('Write your first name', form.text)
+        self.form_field_test_with_callback(callback)
+
+    def test_empty_last_name_error_message(self):
+        def callback(form):
+            last_name_field = self.get_by_placeholder(form, 'Ex.: John')
+            last_name_field.send_keys(' ')
+            last_name_field.send_keys(Keys.ENTER)
+            form = self.get_form()
+            self.assertIn('Escreva seu sobrenome', form.text)
+        self.form_field_test_with_callback(callback)
+
+    def test_empty_username_error_message(self):
+        def callback(form):
+            username_field = self.get_by_placeholder(
+                form, 'Seu apelido')
+            username_field.send_keys(' ')
+            username_field.send_keys(Keys.ENTER)
+            form = self.get_form()
+            self.assertIn('Este campo não deve estar vazio', form.text)
+        self.form_field_test_with_callback(callback)
+
+    def test_invalid_email_error_message(self):
+        def callback(form):
+            email_field = self.get_by_placeholder(form, 'Seu e-mail')
+            email_field.send_keys('email@invalid')
+            email_field.send_keys(Keys.ENTER)
+            form = self.get_form()
+            self.assertIn('O e-mail deve ser válido.', form.text)
+        self.form_field_test_with_callback(callback)
+
+    def test_passwords_do_not_match(self):
+        def callback(form):
+            password1 = self.get_by_placeholder(form, 'Sua senha')
+            password2 = self.get_by_placeholder(form, 'Confirme sua senha')
+            password1.send_keys('P@ssw0rd')
+            password2.send_keys('P@ssw0rd_Different')
+            password2.send_keys(Keys.ENTER)
+            form = self.get_form()
+            self.assertIn('Senha e senha2 devem ser iguais', form.text)
+        self.form_field_test_with_callback(callback)
+
+    def test_user_valid_data_register_successfully(self):
+        self.browser.get(self.live_server_url + '/authors/register/')
+        form = self.get_form()
+
+        self.get_by_placeholder(form, 'Ex.: John').send_keys('Primeiro nome')
+        self.get_by_placeholder(form, 'Ex.: da Silva').send_keys('Sobrenome')
+        self.get_by_placeholder(form, 'Seu apelido').send_keys('my_username')
+        self.get_by_placeholder(
+            form, 'Seu e-mail').send_keys('email@valid.com')
+        self.get_by_placeholder(
+            form, 'Sua senha').send_keys('P@ssw0rd1')
+        self.get_by_placeholder(
+            form, 'Confirme sua senha').send_keys('P@ssw0rd1')
+
+        form.submit()
+
+        self.assertIn(
+            'Acesso criado, por favor faça login.',
+            self.browser.find_element(By.TAG_NAME, 'body').text
         )
-
-        self.assertIn('Escreva seu primeiro nome', form.text)
