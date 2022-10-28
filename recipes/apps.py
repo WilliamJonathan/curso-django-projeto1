3f720eed0c091e99fb904bb900819c954f5f14a7
Jonathan William da Silva <jonathanwilliam.ws@gmail.com>
2022-10-28 16:33:10 -0300

    tratamento para excluir imagens

--- recipes/apps.py
+++ recipes/apps.py
@@ -5,3 +5,8 @@ class RecipesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'recipes'
     verbose_name = 'Receitas'
+
+    def ready(self, *args, **kwargs) -> None:
+        import recipes.signals  # noqa
+        super_ready = super().ready(*args, **kwargs)
+        return super_ready
