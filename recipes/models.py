4dd650c335e5131fd4ac00f9aa7b6a1fe33c1ae4
Jonathan William da Silva <jonathanwilliam.ws@gmail.com>
2022-11-07 10:44:32 -0300

    serializers models

--- recipes/models.py
+++ recipes/models.py
@@ -4,10 +4,13 @@ from collections import defaultdict
 from django.conf import settings
 from django.contrib.auth.models import User
 from django.db import models
+from django.db.models import F, Value
+from django.db.models.functions import Concat
 from django.forms import ValidationError
 from django.urls import reverse
 from django.utils.text import slugify
 from PIL import Image
+
 from tag.models import Tag
 
 
@@ -22,7 +25,24 @@ class Category(models.Model):
         verbose_name_plural = 'Categorias'
 
 
+class RecipeManager(models.Manager):
+    def get_published(self):
+        return self.filter(
+            is_published=True
+        ).annotate(
+            author_full_name=Concat(
+                F('author__first_name'), Value(' '),
+                F('author__last_name'), Value(' ('),
+                F('author__username'), Value(')'),
+            )
+        )\
+            .order_by('-id') \
+            .select_related('category', 'author') \
+            .prefetch_related('tags')
+
+
 class Recipe(models.Model):
+    objects = RecipeManager()
     title = models.CharField(max_length=65, verbose_name='titulo')
     description = models.CharField(max_length=165, verbose_name='descrição')
     slug = models.SlugField(unique=True)
